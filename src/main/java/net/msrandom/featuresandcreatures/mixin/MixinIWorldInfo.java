7e95f6d02c415a7369353ca1206f6b3654926010
chaottic <64570529+chaottic@users.noreply.github.com>
2022-03-20 16:03:27 +1000

    Goodbye everything

--- src/main/java/net/msrandom/featuresandcreatures/mixin/MixinIWorldInfo.java
+++
@@ -1,9 +0,0 @@
-package net.msrandom.featuresandcreatures.mixin;
-
-import net.minecraft.world.storage.IWorldInfo;
-import net.msrandom.featuresandcreatures.entity.spawner.FnCSpawnerLevelContext;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(IWorldInfo.class)
-public interface MixinIWorldInfo extends FnCSpawnerLevelContext {
-}
